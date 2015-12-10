3dcccbb431adc210ac07daf3fe390fe977cf69ad
Jorge <floresjmjr@gmail.com>
2015-12-09 23:03:13 -0500

    added changes to users_controller and sessions_helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
 
